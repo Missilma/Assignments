31a043613a7c7f414938c63aa7aaeb2114764e1a
Missilma <is2220047@gmail.com>
2022-07-16 15:03:35 +0530

    Assignment-Day-Two

--- App.js
+++ App.js
@@ -1,13 +1,47 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import { StyleSheet, Text, View } from 'react-native';
+import { Button } from 'react-native-web';
+
+export default class App extends React.Component {
+  state = {
+    value: 0,
+    
+  }
+
+  increaseValue = () => {
+    this.setState({
+      value: this.state.value + 1,
+      tatal_taps: this.state.total_taps + 1
+    })
+    console.log("value: " + (this.state.value + 1))
+  };
+
+  decreaseValue = () => {
+    this.setState({
+      value: this.state.value - 1,
+      tatal_taps: this.state.total_taps + 1
+    })
+    console.log("value: " + (this.state.value - 1))
+  };
+
+  render() {
+    const {value} = this.state
+    return (
+      <View style={styles.container}>
+        <Text style={{ fontSize: 60, marginBottom: -20 }}>{value}</Text>
+        <Text style={{ fontSize: 12, padding: 20, color: 'grey' }}>-12</Text>
+        <Text style={{ fontSize: 12, padding: 20, color: 'grey' }}>{"Limit Reached"}</Text>
+        <StatusBar style='auto'/>
+        <View style={{ flexDirection: 'row' }}>
+          <Button onPress={this.decreaseValue} title="Decrement" />
+          <Text>   </Text>
+          <Button onPress={this.increaseValue} title="Increment" />
+        </View>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
@@ -16,5 +50,6 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+
   },
 });
